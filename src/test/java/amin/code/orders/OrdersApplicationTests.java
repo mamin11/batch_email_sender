205670abae1bf6a3ba1a09f33351b94a21bef46a
mamin11 <abdimamin@gmail.com>
2021-12-18 22:36:15 +0000

    initial commit

---
+++ src/test/java/amin/code/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package amin.code.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
